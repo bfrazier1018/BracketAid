f97db9ff6b2842165df2696b7f6f7bdee7cff046
Brandon Frazier <bfrazier1018@gmail.com>
2016-05-07 10:05:08 -0400

    Add loop through Sportradar API to return and create a Name, Team_Id, and Seed for each team

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,11 +1,20 @@
 class TeamController < ApplicationController
-
+	
+	require "httparty"
 	protect_from_forgery with: :null_session
 
 	def index
 
 		response = HTTParty.get("http://api.sportradar.us/ncaamb-t3/tournaments/608152a4-cccc-4569-83ac-27062580099e/summary.json?api_key=t8533xedsh9yewxfka46mq5u")
-		body = JSON.parse(response.body)
-		render json: body["brackets"], status:200
+		body = response.parsed_response["brackets"]
+		
+		body.each do |bracket|
+			bracket["participants"].each do |team|
+				Team.create(team_id: team["id"], name: team["market"], seed: team["seed"])
+			end
+		end		
+
+		render json: "success"
 	end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506205351) do
+ActiveRecord::Schema.define(version: 20160507013818) do
 
   create_table "apis", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer  "team_id"
+    t.string   "name"
+    t.integer  "seed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
