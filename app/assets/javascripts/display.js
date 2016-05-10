18c63ae9a7ac7d3b1e6df49d6b1a0ad18c725485
Brandon Frazier <bfrazier1018@gmail.com>
2016-05-10 15:16:44 -0400

    Update Table Stats with personal database

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -21,51 +21,47 @@ function displayInfoTeam2(info) {
 
 // ----------------- DISPLAY TEAM STATS IN TABLE ---------------------
 function displayStatsTeam1(info) {
-	var team = info.stats.own_record.average;
-	var opponent = info.stats.opponents.average;
 
 	$(".js-rank-team1").text(info.rank.rank);
 	$(".js-sos-team1").text(info.rank.sos);
 	$(".js-rpi-team1").text(info.rank.rpi);
 	$(".js-table-name-team1").text(info.user.name);
-	$(".js-ppg-team1").text(team.points);
-	$(".js-opponent-ppg-team1").text(opponent.points);
-	$(".js-paint-points-team1").text(team.paint_pts);
-	$(".js-opponent-paint-points-team1").text(opponent.paint_pts);
-	$(".js-threes-team1").text(team.three_points_made);
-	$(".js-rebounds-team1").text(team.rebounds);
-	$(".js-offensive-rebounds-team1").text(team.off_rebounds);
-	$(".js-steals-team1").text(team.steals);
-	$(".js-turnovers-team1").text(team.turnovers);
-	$(".js-free-throws-team1").text((team.free_throws_made / team.free_throws_att).toFixed(2) * 100);
-	$(".js-field-goal-team1").text((team.field_goals_made / team.field_goals_att).toFixed(2) * 100)
-	$(".js-opponent-field-goal-team1").text((opponent.field_goals_made / opponent.field_goals_att).toFixed(2) * 100)
-	$(".js-opponent-threes-team1").text(opponent.three_points_made);
-	$(".js-scoring-margin-team1").text((team.points - opponent.points).toFixed(2));
+	$(".js-ppg-team1").text(info.stats.points);
+	$(".js-opponent-ppg-team1").text(info.stats.opponent_points);
+	$(".js-paint-points-team1").text(info.stats.paint_pts);
+	$(".js-opponent-paint-points-team1").text(info.stats.opponent_paint_pts);
+	$(".js-threes-team1").text(info.stats.three_points_made);
+	$(".js-rebounds-team1").text(info.stats.rebounds);
+	$(".js-offensive-rebounds-team1").text(info.stats.off_rebounds);
+	$(".js-steals-team1").text(info.stats.steals);
+	$(".js-turnovers-team1").text(info.stats.turnovers);
+	$(".js-free-throws-team1").text((info.stats.free_throws_made / info.stats.free_throws_att).toFixed(2) * 100);
+	$(".js-field-goal-team1").text((info.stats.field_goals_made / info.stats.field_goals_att).toFixed(2) * 100)
+	$(".js-opponent-field-goal-team1").text((info.stats.opponent_field_goals_made / info.stats.opponent_field_goals_att).toFixed(2) * 100)
+	$(".js-opponent-threes-team1").text(info.stats.opponent_three_points_made);
+	$(".js-scoring-margin-team1").text((info.stats.points - info.stats.opponent_points).toFixed(2));
 };
 
 function displayStatsTeam2(info) {
-	var team = info.stats.own_record.average;
-	var opponent = info.stats.opponents.average;
-
+	
 	$(".js-rank-team2").text(info.rank.rank);
 	$(".js-sos-team2").text(info.rank.sos);
 	$(".js-rpi-team2").text(info.rank.rpi);
 	$(".js-table-name-team2").text(info.user.name);
-	$(".js-ppg-team2").text(team.points);
-	$(".js-opponent-ppg-team2").text(opponent.points);
-	$(".js-paint-points-team2").text(team.paint_pts);
-	$(".js-opponent-paint-points-team2").text(opponent.paint_pts);
-	$(".js-threes-team2").text(team.three_points_made);
-	$(".js-rebounds-team2").text(team.rebounds);
-	$(".js-offensive-rebounds-team2").text(team.off_rebounds);
-	$(".js-steals-team2").text(team.steals);
-	$(".js-turnovers-team2").text(team.turnovers);
-	$(".js-free-throws-team2").text((team.free_throws_made / team.free_throws_att).toFixed(2) * 100);
-	$(".js-field-goal-team2").text((team.field_goals_made / team.field_goals_att).toFixed(2) * 100)
-	$(".js-opponent-field-goal-team2").text((opponent.field_goals_made / opponent.field_goals_att).toFixed(2) * 100)
-	$(".js-opponent-threes-team2").text(opponent.three_points_made);
-	$(".js-scoring-margin-team2").text((team.points - opponent.points).toFixed(2));
+	$(".js-ppg-team2").text(info.stats.points);
+	$(".js-opponent-ppg-team2").text(info.stats.opponent_points);
+	$(".js-paint-points-team2").text(info.stats.paint_pts);
+	$(".js-opponent-paint-points-team2").text(info.stats.opponent_paint_pts);
+	$(".js-threes-team2").text(info.stats.three_points_made);
+	$(".js-rebounds-team2").text(info.stats.rebounds);
+	$(".js-offensive-rebounds-team2").text(info.stats.off_rebounds);
+	$(".js-steals-team2").text(info.stats.steals);
+	$(".js-turnovers-team2").text(info.stats.turnovers);
+	$(".js-free-throws-team2").text((info.stats.free_throws_made / info.stats.free_throws_att).toFixed(2) * 100);
+	$(".js-field-goal-team2").text((info.stats.field_goals_made / info.stats.field_goals_att).toFixed(2) * 100)
+	$(".js-opponent-field-goal-team2").text((info.stats.opponent_field_goals_made / info.stats.opponent_field_goals_att).toFixed(2) * 100)
+	$(".js-opponent-threes-team2").text(info.stats.opponent_three_points_made);
+	$(".js-scoring-margin-team2").text((info.stats.points - info.stats.opponent_points).toFixed(2))
 
 };
 
